170810ce423589508b47f59325682ccb872dbfc1
Mango <mstzf@foxmail.com>
2024-11-15 12:01:07 +0800

    meta

--- header_com.php
+++ header_com.php
@@ -19,7 +19,7 @@
     'search'    =>  _t('包含关键字 %s 的文章'),
     'tag'       =>  _t('标签 %s 下的文章'),
     'author'    =>  _t('%s 发布的文章')
-  ), '', ' - '); ?><?php $this->options->title(); ?>">
+  ), '', ' | '); ?><?php $this->options->title(); ?>">
   <?php if ($this->isSingle()): ?>
   <meta property="og:description" content="<?php summaryContent($this); ?>">
   <?php endif; ?>
